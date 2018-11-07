5a6b4e9023e5c597e56e8d60f785fc38b850652c
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-07 10:17:56 -0500

    added new pics, notes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './suckerberg.png';
+import logo from './suckerberg3.png';
 import './App.css';
 
 class Text extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return (
-      <div style={{color: this.props.color}}>{this.props.value}</div>
-    );
+    return <div style={{color: this.props.color}}>{this.props.value}</div>;
   }
 };
+
+// render() is a "lifecycle method"
+  // react library controls it, you just write the instructions for how the components behave/look
+  // react creates the tags and puts them into the DOM for us
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Text color="#61dafb" id="sucker" value="Mark Zuckerberg Suckerbergs"></Text>
+        <p id="sucker"><Text color="#61dafb" value="Mark Zuckerberg Suckerbergs"></Text></p>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
